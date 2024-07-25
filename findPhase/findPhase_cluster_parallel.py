4726c1e15b71bf4cd9beafd5d70347bcb9c963d2
NightRunner7 <krzysztof.rafal.szafranski@gmail.com>
2024-07-25 09:32:44 +0200

    Create findPhase_cluster_parallel.py

---
+++ findPhase/findPhase_cluster_parallel.py
@@ -0,0 +1,118 @@
+import os
+import sys
+import argparse
+import numpy as np
+from multiprocessing import Pool
+# --- IMPORT FROM FILES
+from assistantFunctions import save_phase_scan_over_connectivity
+# Add the parent directory to the Python module search path
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
+import config as cfg
+from TwitterRadicalizationModel import TwitterRadicalizationModel
+# --- SET INITIAL GRAP !!!
+from initialGraph.watts_NS_UW import create_graph, create_name
+
+
+# ---------------------------------------- SIMULATE WITH FIXED PARAMS ------------------------------------------------ #
+def simulate_phase_point(radical_members, k, probability, val_D, val_Deff, run, time):
+    # --- FIXED PARAMETERS: USER
+    sim_config = {
+        # Base parameters
+        'members': 1000,
+        'radical_members': radical_members,
+        'k': k,
+        'p': probability,
+        # Weights distribution, set zero if they not normal distributed
+        'mean': 0.5,
+        'std_dev': 0.05,
+        # Diffusion and effective diffusion
+        'D': val_D,
+        'Deff': val_Deff,
+        # Other
+        'run': run
+    }
+
+    # Manage time settings
+    time_end = time
+    dt = 0.001
+    number_of_plots_and_checks = time
+    time_moment = 0.0  # set time of reaching stable phase
+
+    # --- FIXED PARAMETERS: AUTOMATIC
+    # set beta parameter
+    sim_config = cfg.set_network_evolution_parameters(sim_config, sim_config['Deff'], diffusion=sim_config['D'])
+
+    # set time step and ending time
+    sim_config = cfg.adjust_time_for_diffusion(sim_config,
+                                               sim_config['D'],
+                                               base_time_end=time_end,
+                                               base_dt=dt,
+                                               check_interval=number_of_plots_and_checks,
+                                               draw_interval=number_of_plots_and_checks,
+                                               update_interval=number_of_plots_and_checks)
+
+    # --- CREATE MODEL
+    init_network = create_graph(set_affiliation_choice=False, sim_config=sim_config)
+
+    TwitterModel = TwitterRadicalizationModel(init_network,
+                                              D=sim_config['D'],
+                                              beta=sim_config['beta'],
+                                              dt=sim_config['dt'])
+
+    # --- EVOLVE NETWORK
+    for step in range(sim_config['timeSteps']):
+        # Periodically check the network's phase status, stop simulation if: achieve stable phase, non-stable network.
+        if step % sim_config['timeStepsToCheck'] == 0:
+            time_moment = step * sim_config['dt']
+            stop_simulation_flag = TwitterModel.stop_simulation_criteria(time_moment)
+            if stop_simulation_flag:
+                break
+
+        # Perform an evolution step in the model.
+        TwitterModel.evolve()
+
+    # --- STORE DATA
+    phase = TwitterModel.return_phase_value()
+    stable_evolution = TwitterModel.return_stable_evolution()
+
+    return [time_moment, phase, stable_evolution]
+
+# ---------------------------------------- MAIN FUNCTION ------------------------------------------------------------- #
+def main(radical_members, probability, val_D, val_Deff, run, time):
+    # Collect data and path/name
+    str_nrad = f"{radical_members}"
+    directory_name = create_name(members=1000, probability=probability,
+                                 mean=0.5, std_dev=0.05,
+                                 D=val_D, Deff=val_Deff,
+                                 run_number=run)
+
+    # Set the range of connectivity
+    # k_arr = np.arange(2, 302, 2)
+    k_arr = np.arange(30, 34, 2)
+    params = [(radical_members, k, probability, val_D, val_Deff, run, time) for k in k_arr]
+
+    with Pool(processes=2) as pool:  # Utilize as many cores as are beneficial
+        results_list = pool.map(simulate_phase_point, params)
+
+    # Prepare arrays to store results
+    time_moment_arr = np.array([result[0] for result in results_list])
+    phase_arr = np.array([result[1] for result in results_list])
+    stable_evolution_arr = np.array([result[2] for result in results_list])
+
+    # Save our data
+    save_phase_scan_over_connectivity(k_arr, phase_arr, time_moment_arr, stable_evolution_arr,
+                                      str_nrad, directory_name, directory_localization="./OutputPhase")
+
+# ---------------------------------------- RUN VIA TERMINAL ---------------------------------------------------------- #
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run network simulation.")
+    parser.add_argument("--radical_members", type=int, required=True)
+    parser.add_argument("--probability", type=float, required=True)
+    parser.add_argument("--val_D", type=float, required=True)
+    parser.add_argument("--val_Deff", type=float, required=True)
+    parser.add_argument("--run", type=int, required=True)
+    parser.add_argument("--time", type=int, required=True)
+
+    args = parser.parse_args()
+    main(args.radical_members, args.probability, args.val_D, args.val_Deff, args.run, args.time)
